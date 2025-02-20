f1de1ea0b474527d7ddc46d16541e83a723dcc88
manojbishwakarma <manojbishwakarma@neebal.com>
2025-02-20 14:32:02 -0500

    Updated song model and controller to handle requests for specific song details by songId.

--- controller/songsController.js
+++ controller/songsController.js
@@ -10,7 +10,25 @@ const getAllSongs = async (req, res) => {
     res.status(500).json({ message: "error getting the songs" + error });
   }
 };
+// Get song by ID
+// Get song by songId
+const getSongById = async (req, res) => {
+  const { songId } = req.params;
+
+  try {
+    const song = await Song.findOne({ songId: songId });
+
+    if (!song) {
+      return res.status(404).json({ message: "Song not found" });
+    }
+
+    res.json(song);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting the song by ID: " + error });
+  }
+};
 
 module.exports = {
   getAllSongs,
+  getSongById,
 };
--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllSongs } = require("../controller/songsController");
+const { getAllSongs, getSongById } = require("../controller/songsController");
 
 // Route to get all songs
 router.get("/getAllSongs", getAllSongs);
+
+// Route to get song by songId
+router.get("/getSongById/:songId", getSongById);
 
 module.exports = router;
