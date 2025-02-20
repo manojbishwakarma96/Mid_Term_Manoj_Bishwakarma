8d6fdbf4b04e9ff86c1bae775a6accd83b69e466
manojbishwakarma <manojbishwakarma@neebal.com>
2025-02-20 14:37:01 -0500

    refactor and handaling the bugs while fetching the specific song by id

--- controller/songsController.js
+++ controller/songsController.js
@@ -10,21 +10,24 @@ const getAllSongs = async (req, res) => {
     res.status(500).json({ message: "error getting the songs" + error });
   }
 };
-// Get song by ID
+
+// Get song by songId
 // Get song by songId
 const getSongById = async (req, res) => {
   const { songId } = req.params;
 
   try {
-    const song = await Song.findOne({ songId: songId });
+    const song = await Song.findOne({ songId });
 
     if (!song) {
       return res.status(404).json({ message: "Song not found" });
     }
 
-    res.json(song);
+    return res.json(song);
   } catch (error) {
-    res.status(500).json({ message: "Error getting the song by ID: " + error });
+    return res
+      .status(500)
+      .json({ message: `Error getting the song by songId: ${error}` });
   }
 };
 
