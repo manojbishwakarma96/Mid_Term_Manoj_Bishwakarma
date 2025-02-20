ffc940f6aadc61264e38b528bf5571217d974870
manojbishwakarma <manojbishwakarma@neebal.com>
2025-02-20 13:36:02 -0500

    created songs.js model also Import songs data from JSON file into MongoDB

--- models/songs.js
+++ models/songs.js
@@ -1,1 +1,16 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+
+const songSchema = new mongoose.Schema({
+  songId: { type: Number, unique: true },
+  artistData: {
+    artistId: { type: Number },
+    name: { type: String },
+    country: { type: String },
+  },
+  releaseDate: { type: String },
+  songTitle: { type: String },
+  genre: { type: String },
+});
+const Song = mongoose.model("Song", songSchema);
+
+module.exports = Song;
--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fs = require("fs");
+
+const Song = require("./models/songs"); // Import the Mongoose model
+
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 
 // Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://manojbishwakarma88:manoj123@songscluster.foh72.mongodb.net/"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
 
-mongoose.connect(
-  "mongodb+srv://manojbishwakarma88:manoj123@moviescluster.qzfcj.mongodb.net/",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+    // Read songs.json file
+    const songsData = JSON.parse(fs.readFileSync("songs.json", "utf-8"));
+
+    // Insert songs into the database
+    Song.insertMany(songsData)
+      .then(() => {
+        console.log("Songs imported successfully");
+        // Close the connection after import
+        mongoose.connection.close();
+      })
+      .catch((err) => {
+        console.error("Error importing songs:", err);
+        mongoose.connection.close();
+      });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 // Start the server
 app.listen(PORT, () => {
